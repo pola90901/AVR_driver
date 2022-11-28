b8fc0b5c815acae1d309ec49b180ea8f43d408b4
pola helmy <117487838+pola90901@users.noreply.github.com>
2022-11-28 12:54:28 +0200

    Delete TOGGLE_LED directory

--- TOGGLE_LED/util.h
+++
@@ -1,6 +0,0 @@
-#ifndef util_h
-#define util_h
-#define set_bit(var,bitpos) var=var |(1<<bitpos)
-#define clear_bit(var,bitpos) var =var &~(1<<bitpos)
-#define toggle_bit(var,bitpos) var=var ^ (1<<bitpos)
-#endif
